208a53eccef38c1a19482d4a4c0ed770b5338c6a
Meadosc <meadosc@onid.orst.edu>
2016-05-15 11:21:15 -0700

    Update ExpressAct.js

--- ExpressAct.js
+++ ExpressAct.js
@@ -1,32 +1,30 @@
 var express = require('express');
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.get('/',function(req,res){
-  res.render('home.handlebars') //We can omit the .handlebars extension as we do below
-  var randNum = Math.random();
-  res.render('Random number between 0 and 1: ' + randNum);
+  res.type('text/plain');
+  res.send('Welcome to the main page!');
 });
 
 app.get('/other-page',function(req,res){
-  res.render('other-page');
+  res.type('text/plain');
+  res.send('Welcome to the other page!');
 });
 
 app.use(function(req,res){
+  res.type('text/plain');
   res.status(404);
-  res.render('404');
+  res.send('404 - Not Found');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.type('plain/text');
   res.status(500);
-  res.render('500');
+  res.send('500 - Server Error');
 });
 
 app.listen(app.get('port'), function(){
